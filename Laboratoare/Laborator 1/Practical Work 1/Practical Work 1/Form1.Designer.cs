a579319c6ebfb990aa94ae02cc7168f321612f4a
Teo <dtie2524@scs.ubbcluj.ro>
2020-03-26 13:39:44 +0200

    first commit

---
+++ Laboratoare/Laborator 1/Practical Work 1/Practical Work 1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Practical_Work_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvClient = new System.Windows.Forms.DataGridView();
+            this.dgvAppointment = new System.Windows.Forms.DataGridView();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClient)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAppointment)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvClient
+            // 
+            this.dgvClient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClient.Location = new System.Drawing.Point(10, 26);
+            this.dgvClient.Name = "dgvClient";
+            this.dgvClient.RowHeadersWidth = 102;
+            this.dgvClient.RowTemplate.Height = 40;
+            this.dgvClient.Size = new System.Drawing.Size(1639, 592);
+            this.dgvClient.TabIndex = 0;
+            this.dgvClient.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dgvAppointment
+            // 
+            this.dgvAppointment.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAppointment.Location = new System.Drawing.Point(2308, 12);
+            this.dgvAppointment.Name = "dgvAppointment";
+            this.dgvAppointment.RowHeadersWidth = 102;
+            this.dgvAppointment.RowTemplate.Height = 40;
+            this.dgvAppointment.Size = new System.Drawing.Size(1678, 657);
+            this.dgvAppointment.TabIndex = 1;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(1702, 237);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(559, 142);
+            this.updateButton.TabIndex = 2;
+            this.updateButton.Text = "Update DB";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(1725, 687);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(563, 147);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(1725, 949);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(563, 147);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(38, 656);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1611, 786);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(3844, 2108);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.dgvAppointment);
+            this.Controls.Add(this.dgvClient);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClient)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAppointment)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvClient;
+        private System.Windows.Forms.DataGridView dgvAppointment;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Laboratoare/Laborator 1/Practical Work 1/Practical Work 1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+using NLog.Internal;
+using System.Collections.Generic;
+
+namespace Practical_Work_1
+{
+
+    // In Windows form app creaza o procedura fillData(Data grid view ca si control- recomanda)
+    // Sql connection
+    //o metoda care da connection string
+    // apoi create new sqlDataAdaptrs -- (unul pt fiecare tabel
+    // create data set (unul pt toate tabelele)
+    //populate the data set (dataele din cele 2 tabele (tabela parinte tabel fiu - din lab 1) si relations between the 2 tables)
+    // un alt obiect de tip realtion pe care il punem in data set(se face in sapt 3) *(conetarerea tabelelor prin relatiile existente -- cheie straina)
+    //afisam datele + add,delete,update
+
+    public partial class Form1 : Form
+    {
+
+        SqlConnection dbConn;
+        SqlDataAdapter daClient, daAppointment;
+        DataSet ds;
+        SqlCommandBuilder cb;
+        BindingSource bsClient, bsAppointment;
+        DataRelation dr;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectToDataBase();
+            ds = new DataSet();
+            fillData();
+            createDataRelation();
+            dataBiding();
+
+
+        }
+
+
+        public void dataBiding()
+        {
+            bsClient = new BindingSource();
+            bsClient.DataSource = ds;
+            bsClient.DataMember = "Client";
+            bsAppointment = new BindingSource();
+            bsAppointment.DataSource = bsClient;
+            bsAppointment.DataMember = "FK__Appointment__CID__6D0D32F4";
+
+            dgvClient.DataSource = bsClient;
+            dgvAppointment.DataSource = bsAppointment;
+        }
+
+        public void createDataRelation() {
+            dr = new DataRelation("FK__Appointment__CID__6D0D32F4",
+                ds.Tables["Client"].Columns["CID"],
+                ds.Tables["Appointment"].Columns["CID"]);
+            ds.Relations.Add(dr);
+        }
+
+        public void fillData() {
+            daClient = new SqlDataAdapter("Select * from Client", dbConn);
+
+/*            daClient.InsertCommand = new SqlCommand("insert into Client(CID, FirstName, LastName, Email, PhoneNumber)" +
+                " values (@CID, @FirstName, @LastName, @Email, @PhoneNumber)", dbConn);
+            daClient.UpdateCommand = new SqlCommand("update client" +
+                                "set FirstName =@FirstName, LastName = @LastName, Email = @Email, PhoneNo = @PhoneNo" +
+                                "where CID = @CID ", dbConn);
+            daClient.DeleteCommand = new SqlCommand("Delete from Client" +
+                                    " where CID = @CID", dbConn);
+            daClient.InsertCommand.Parameters.Add("@CID", SqlDbType.Int, 5, "CID");*/
+
+            daAppointment = new SqlDataAdapter("SELECT * from Appointment", dbConn);
+
+/*            daAppointment.InsertCommand = new SqlCommand("insert into Appointment (CID,SID,EmID,TyID,AtID,Duration,Price,Date) " +
+                "values (@CID,@SID,@EmID,@TyID,@AtID,@Duration,@Price,@Date)", dbConn);
+            daAppointment.DeleteCommand = new SqlCommand("DELETE FROM Appointment WHERE CID = @CID, SID = @SID, Date = @Date", dbConn);
+            daAppointment.UpdateCommand = new SqlCommand("Update Appointment " +
+                "set Duration = @Duration, Price = @Price, Date = @Date" +
+                "where CID = @CID, SID = @SID, EmID = @EmID, TyID = @TyID , AtID = @AtID", dbConn);*/
+            cb = new SqlCommandBuilder(daAppointment);
+            daClient.Fill(ds, "Client");
+            daAppointment.Fill(ds, "Appointment");
+            cb.GetUpdateCommand();
+
+        }
+
+        public void connectToDataBase() { 
+            dbConn = new SqlConnection("Data Source = DESKTOP-KCS7V0D\\SQLEXPRESS; Initial Catalog = Nail_Salon;" + "Integrated Security = true;");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        { //update
+            try
+            {
+                daAppointment.Update(ds, "Appointment");
+                e.ToString();
+            }
+            catch (InvalidOperationException d) {
+                d.ToString();
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                daAppointment.Update(ds, "Appointment");
+            }
+            catch (InvalidOperationException d )
+            {
+                d.ToString();
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                daAppointment.Update(ds, "Appointment");
+            }
+            catch (InvalidOperationException d)
+            {
+                d.ToString();
+            }
+
+
+        }
+    }
+}
